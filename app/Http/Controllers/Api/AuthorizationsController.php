38a2b6642a68462e61b33a75e9ebf08a4f6db54d
vagrant <vagrant@homestead>
2018-09-12 06:32:41 +0000

    add jwt

--- app/Http/Controllers/Api/AuthorizationsController.php
+++ app/Http/Controllers/Api/AuthorizationsController.php
@@ -2,15 +2,34 @@
 
 namespace App\Http\Controllers\Api;
 
+use Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\Api\AuthorizationRequest;
 use App\Http\Requests\Api\SocialAuthorizationRequest;
 
 class AuthorizationsController extends Controller
 {
-    public function socialStore($type, SocialAuthorizationRequest $request)
+    public function store (AuthorizationRequest $request)
     {
-        if (!in_array($type, ['weixin'])) {
+        $username = $request->username;
+        
+        filter_var($username , FILTER_VALIDATE_EMAIL) ?
+            $credentials['email'] = $username :
+            $credentials['phone'] = $username;
+        
+        $credentials['password'] = $request->password;
+        
+        if ( !$token = Auth::guard('api')->attempt($credentials)) {
+            return $this->response->errorUnauthorized('用户名或密码错误');
+        }
+        
+        return $this->respondWithToken($token)->setStatusCode(201);
+    }
+    
+    public function socialStore ($type , SocialAuthorizationRequest $request)
+    {
+        if ( !in_array($type , [ 'weixin' ])) {
             return $this->response->errorBadRequest();
         }
         
@@ -19,7 +38,7 @@ class AuthorizationsController extends Controller
         try {
             if ($code = $request->code) {
                 $response = $driver->getAccessTokenResponse($code);
-                $token = array_get($response, 'access_token');
+                $token = array_get($response , 'access_token');
             } else {
                 $token = $request->access_token;
                 
@@ -38,24 +57,47 @@ class AuthorizationsController extends Controller
                 $unionid = $oauthUser->offsetExists('unionid') ? $oauthUser->offsetGet('unionid') : null;
                 
                 if ($unionid) {
-                    $user = User::where('weixin_unionid', $unionid)->first();
+                    $user = User::where('weixin_unionid' , $unionid)->first();
                 } else {
-                    $user = User::where('weixin_openid', $oauthUser->getId())->first();
+                    $user = User::where('weixin_openid' , $oauthUser->getId())->first();
                 }
                 
                 // 没有用户，默认创建一个用户
-                if (!$user) {
+                if ( !$user) {
                     $user = User::create([
-                        'name' => $oauthUser->getNickname(),
-                        'avatar' => $oauthUser->getAvatar(),
-                        'weixin_openid' => $oauthUser->getId(),
-                        'weixin_unionid' => $unionid,
+                        'name'           => $oauthUser->getNickname() ,
+                        'avatar'         => $oauthUser->getAvatar() ,
+                        'weixin_openid'  => $oauthUser->getId() ,
+                        'weixin_unionid' => $unionid ,
                     ]);
                 }
                 
                 break;
         }
         
-        return $this->response->array(['token' => $user->id]);
+        $token = Auth::guard('api')->fromUser($user);
+        return $this->respondWithToken($token)->setStatusCode(201);
+
+    }
+    
+    protected function respondWithToken($token)
+    {
+        return $this->response->array([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60
+        ]);
+    }
+    
+    public function update()
+    {
+        $token = Auth::guard('api')->refresh();
+        return $this->respondWithToken($token);
+    }
+    
+    public function destroy()
+    {
+        Auth::guard('api')->logout();
+        return $this->response->noContent();
     }
 }
